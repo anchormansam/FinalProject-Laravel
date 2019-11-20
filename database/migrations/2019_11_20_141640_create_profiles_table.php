0bed22b5cab3d87509a3967a6bcbffa125dd0b38
Sam Wheeling <anchormansam@gmail.com>
2019-11-20 10:17:49 -0500

    first story done
    -user can login
    -user can logout

---
+++ database/migrations/2019_11_20_141640_create_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('name');
+            $table->text('address');
+            $table->text('city');
+            $table->text('state');
+            $table->integer('zipcode');
+            $table->boolean('admin');
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
